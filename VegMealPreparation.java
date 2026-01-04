e51e7fe9f02f24c6baddf980f51ffb7957422338
fatiha693 <167349551+fatiha693@users.noreply.github.com>
2026-01-04 17:00:01 +0600

    Implement VegMealPreparation class for meal prep

---
+++ VegMealPreparation.java
@@ -0,0 +1,10 @@
+public class VegMealPreparation extends MealPreparationTemplate {
+protected void washIngredients() {
+System.out.println("Washing vegetables");
+}
+
+
+protected void cookMainItem() {
+System.out.println("Cooking vegetables");
+}
+}
